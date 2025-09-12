db161859fa24083d2b2144f1b7da5bc959a4dabb
Yonaki51 <yonivandon@hotmail.com>
2025-09-12 11:33:46 +0200

    eerste commit

---
+++ src/mastermind/Mastermind.java
@@ -0,0 +1,22 @@
+package mastermind;
+
+import java.util.Random;
+
+public class Mastermind {
+
+    public static void main(String[] args) {
+        String[] colours = {"red", "blue", "yellow", "purple", "green", "orange"};
+        Random rng = new Random();
+
+        System.out.println("geheime code: ");
+        for (int clr_combo = 0; clr_combo <4; clr_combo++) {
+            int index = rng.nextInt(colours.length);
+            String chosen =  colours[index];
+            System.out.print(chosen + " ");
+
+        }
+
+
+    }
+
+}
