7df1b4775382a5a20d026871388a47d8b5d15261
Yonaki51 <yonivandon@hotmail.com>
2025-09-23 14:43:29 +0200

    code gschreven om de random combinatie te maken

--- src/mastermind/Mastermind.java
+++ src/mastermind/Mastermind.java
@@ -1,20 +1,28 @@
 package mastermind;
 
-import java.util.*;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Mastermind {
-    private static final int code_length = 4;
-        private static final Set<String> colors = Set.of(
-            "red", "blue", "yellow", "purple", "green", "orange"
-    );
-
     public static void main(String[] args) {
-        System.out.println("mastermind!");
-        System.out.println("Toegestane kleuren: " + String.join(", ", colors));
-        
+        String[] kleuren = { "rood", "blauw", "groen", "geel", "oranje", "paars" };
+        // geheimde code
+        String[] code = new String[4];
 
-  
+        Random random = new Random();
+        // willekeurige code genereren
+        for (int i = 0; i < code.length; i++) {
+            int index = random.nextInt(kleuren.length);
+            code[i] = kleuren[index];
+
+        }
+        for (String kleur : code) {
+            System.out.print(kleur + " ");
+        }
+
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println("Voer 4 kleuren in, gescheiden door spaties (keuze: rood, blauw, groen, geel, oranje, paars):");
+
     }
-
 
 }
