fc0a1ea5d661b3591f9ed2ef12ad2ce08e278a05
Yonaki51 <yonivandon@hotmail.com>
2025-09-25 13:53:51 +0200

    check toegevoegd die kijkt of de kleur ergens anders in de code wel voorkomt

--- src/mastermind/Mastermind.java
+++ src/mastermind/Mastermind.java
@@ -29,14 +29,14 @@ public class Mastermind {
         // zorg ervoor dat de invoer gesplitst wordt op één of meer spaties
         String[] gok = invoer.split("\\s+");
 
-        int exactgoed = 0;
+        int exactGoed = 0;
         for (int i = 0; i < code.length && i < gok.length; i++) {
             if (gok[i].equalsIgnoreCase(code[i])) {
-                exactgoed++;
+                exactGoed++;
             }
 
         }
-        System.out.println("\nExact goed (kleur + positie): " + exactgoed);
+        System.out.println("\nExact goed (kleur + positie): " + exactGoed);
 
         // (debug) laat geheime code zien
         System.out.println("(debug) Geheime code: " + String.join(" ", code));
@@ -50,9 +50,29 @@ public class Mastermind {
             if (gokCopy[i].equalsIgnoreCase(codeCopy[i])) {
                 codeCopy[i] = null;
                 gokCopy[i] = null;
-                
+
             }
         }
+
+        int verkeerdePlek = 0;
+        for (int i = 0; i < gokCopy.length; i++) {
+            // alleen de niet exacte matches
+            if (gokCopy[i] != null) {
+                for (int j = 0; j < codeCopy.length; j++) {
+                    if (codeCopy[j] != null && gokCopy[i].equalsIgnoreCase(codeCopy[j])) {
+
+                        verkeerdePlek++;
+                        codeCopy[j] = null;
+                        break;
+                    }
+                }
+
+            }
+
+        }
+
+        System.out.println("Exact goed: " + exactGoed);
+        System.out.println("Goede kleur, verkeerde plek: " + verkeerdePlek);
 
     }
 
