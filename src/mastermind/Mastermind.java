94c3cc4e746a1bd49a5b60409f03ce697159f6a7
Yonaki51 <yonivandon@hotmail.com>
2025-09-25 14:42:33 +0200

    een while loop in het script gezet en het aantal maximale beurten toegevoegd

--- src/mastermind/Mastermind.java
+++ src/mastermind/Mastermind.java
@@ -16,57 +16,70 @@ public class Mastermind {
             code[i] = kleuren[index];
 
         }
-        for (String kleur : code) {
-            System.out.print(kleur + " ");
-        }
+
+        int maxBeurten = 10;
+        boolean gewonnen = false;
+        Scanner scanner = new Scanner(System.in);
 
         // laat speler raden
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Voer 4 kleuren in, gescheiden door spaties\n"
-                + "(keuze: rood, blauw, groen, geel, oranje, paars):");
-        String invoer = scanner.nextLine();
+        for (int beurt = 1; beurt <= maxBeurten; beurt++) {
+            System.out.println("Voer 4 kleuren in, gescheiden door spaties\n"
+                    + "(keuze: rood, blauw, groen, geel, oranje, paars):");
+            String invoer = scanner.nextLine();
 
-        // zorg ervoor dat de invoer gesplitst wordt op één of meer spaties
-        String[] gok = invoer.split("\\s+");
+            // zorg ervoor dat de invoer gesplitst wordt op één of meer spaties
+            String[] gok = invoer.trim().split("\\s+");
 
-        // feedback voorbereiden in de vorm van strings
-        StringBuilder exactGoedKleuren = new StringBuilder();
-        StringBuilder verkeerdePlekKleuren = new StringBuilder();
+            // feedback voorbereiden in de vorm van strings
+            StringBuilder exactGoedKleuren = new StringBuilder();
+            StringBuilder verkeerdePlekKleuren = new StringBuilder();
 
-        String[] codeCopy = code.clone();
-        String[] gokCopy = gok.clone();
+            String[] codeCopy = code.clone();
+            String[] gokCopy = gok.clone();
 
-        // Stap 1: exacte matches
-        int exactGoed = 0;
-        for (int i = 0; i < codeCopy.length && i < gokCopy.length; i++) {
-            if (gokCopy[i].equalsIgnoreCase(codeCopy[i])) {
-                exactGoed++;
-                exactGoedKleuren.append(gokCopy[i]).append(" ");
-                codeCopy[i] = null;
-                gokCopy[i] = null;
+            // Stap 1: exacte matches
+            int exactGoed = 0;
+            for (int i = 0; i < codeCopy.length && i < gokCopy.length; i++) {
+                if (gokCopy[i].equalsIgnoreCase(codeCopy[i])) {
+                    exactGoed++;
+                    exactGoedKleuren.append(gokCopy[i]).append(" ");
+                    codeCopy[i] = null;
+                    gokCopy[i] = null;
+                }
             }
-        }
 
-        // Stap 2: verkeerde plek
-        int verkeerdePlek = 0;
-        for (int i = 0; i < gokCopy.length; i++) {
-            if (gokCopy[i] != null) {
-                for (int j = 0; j < codeCopy.length; j++) {
-                    if (codeCopy[j] != null && gokCopy[i].equalsIgnoreCase(codeCopy[j])) {
-                        verkeerdePlek++;
-                        verkeerdePlekKleuren.append(gokCopy[i]).append(" ");
-                        codeCopy[j] = null;
-                        break;
+            // Stap 2: verkeerde plek
+            int verkeerdePlek = 0;
+            for (int i = 0; i < gokCopy.length; i++) {
+                if (gokCopy[i] != null) {
+                    for (int j = 0; j < codeCopy.length; j++) {
+                        if (codeCopy[j] != null && gokCopy[i].equalsIgnoreCase(codeCopy[j])) {
+                            verkeerdePlek++;
+                            verkeerdePlekKleuren.append(gokCopy[i]).append(" ");
+                            codeCopy[j] = null;
+                            break;
+                        }
                     }
                 }
             }
+
+            // Feedback printen
+            System.out.println("(debug) Geheime code: " + String.join(" ", code));
+            System.out.println("Exact goed: " + exactGoed + " -> " + exactGoedKleuren);
+            System.out.println("Goede kleur, verkeerde plek: " + verkeerdePlek + " -> " + verkeerdePlekKleuren);
+
+            if (exactGoed == code.length) {
+                System.out.println("gefeliciteerdQ je hebt de code gekraakt!");
+                gewonnen = true;
+                break;
+            }
+
         }
 
-        // Feedback printen
-        System.out.println("(debug) Geheime code: " + String.join(" ", code));
-        System.out.println("Exact goed: " + exactGoed + " -> " + exactGoedKleuren);
-        System.out.println("Goede kleur, verkeerde plek: " + verkeerdePlek + " -> " + verkeerdePlekKleuren);
-
+        if (!gewonnen) {
+            System.out.println("helaas, je hebt de code niet kunne kraken\n" 
+            + "de geheime code was: " + String.join(" ", code));
+        }
     }
 
 }
